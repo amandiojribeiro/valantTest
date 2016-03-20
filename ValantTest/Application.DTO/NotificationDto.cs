da79da6d9e2b220340d34f85810e5b441388be57
amandiojribeiro <amandiojribeiro@sapo.pt>
2016-03-20 15:19:58 +0000

    add package client.sdk file

---
+++ ValantTest/Application.DTO/NotificationDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ValantTest.Application.DTO
+{
+    using System;
+
+    /// <summary>
+    /// I implemented a object with the same properties that our Model Object has, only for simplicity reasons
+    /// </summary>
+    public class NotificationDto
+    {
+        public Guid Id { get; set; }
+
+        public int Type { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime NotificationDate { get; set; }
+    }
+}
---
+++ ValantTest/Application.Services/InventoryService/InventoryService.cs
@@ -0,0 +1,66 @@
+﻿namespace ValantTest.Application.Services.InventoryService
+{
+    using System.Threading.Tasks;
+    using Domain.Core.TypedRepositories;
+    using Domain.Model;
+    using DTO;
+    using Infrastructure.CrossCutting.Adapters;
+    using Domain.Core.TypedGateways;
+    using System;
+
+    public class InventoryService : IInventoryService
+    {
+        private readonly IItemRepository itemRepository;
+
+        private readonly ISignalRGateway gateway;
+
+        private readonly INotificationRepository notificationRepository;
+
+        public InventoryService(INotificationRepository notificationRepository, IItemRepository itemRepository, ISignalRGateway gateway)
+        {
+            this.itemRepository = itemRepository;
+            this.gateway = gateway;
+            this.notificationRepository = notificationRepository;
+        }
+
+        public async Task<ItemDto> SaveItem(ItemDto request)
+        {
+            var resultItem = await this.itemRepository.GetItemByLablelAsync(request.Label);
+            if (resultItem == null)
+            {
+                request.Count = 0;
+                var itemToInsert = TypeAdapterHelper.Adapt<Item>(request);
+                itemToInsert.Count = 1;
+                var result = await this.itemRepository.SaveItemAsync(itemToInsert);
+                return TypeAdapterHelper.Adapt<ItemDto>(result);
+            }
+            else
+            {
+                resultItem.Count += 1;
+                await this.itemRepository.UpdateAsync(resultItem);
+                return TypeAdapterHelper.Adapt<ItemDto>(resultItem);
+            }
+        }
+
+        public async Task<ItemDto> TakeItemByLabel(string label)
+        {
+            var result = await this.itemRepository.GetItemByLablelAsync(label);
+            if (result != null)
+            {
+                result.Count -= 1;
+                if (result.Count > 0)
+                {
+                    await this.itemRepository.UpdateAsync(result);
+                }
+                else
+                {
+                    await this.itemRepository.DeleteAsync(result);
+                }
+                var notification = new Notification { Id = Guid.NewGuid(), Description = string.Format("Item {0} has been taken!<br/>There are {1} left;", label, result.Count), NotificationDate = DateTime.UtcNow, Type = 2 };
+                await this.notificationRepository.SaveNotificationAsync(notification);
+                await this.gateway.SendMessage(notification);
+            }
+            return TypeAdapterHelper.Adapt<ItemDto>(result);
+        }
+    }
+}
